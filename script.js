772b4835552b91af51af7f0e8d66b7b9951b67ef
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-03-20 17:56:11 +0100

    test js

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 // const myArr = [];
 
 // console.log({ myArr: myArr });
+// function randomColor() {
+//   const letters = "0123456789ABCDEF";
+//   let color = "#";
+
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+
+//   return color;
+// }
+
+// console.log(randomColor());
+// console.log(randomColor());
+// console.log(randomColor());
+
+// const text = "Ala ma kota";
+
+// //sprawdzamy czy ciąg "psa" istnieje
+// if (text.indexOf("psa") !== -1) {
+//   console.log("Ala ma kota");
+// } else {
+//   console.log("Ala ma psa");
+// }
+
+// const url = "http://nazwastrony.pl/przykladowaNazwaPliku.php";
+
+// const file = url.slice(url.lastIndexOf("/") + 1); //"przykladowaNazwaPliku.php"
+// console.log(file);
+// const part = file.split("."); //dzielimy nazwę na części (otrzymujemy tablicę)
+// console.log(part);
+// console.log(part[1]); //"php"
+// console.log(part[0]);
+
+// const txt = "abecadło";
+
+// for (let i = 0; i < txt.length; i++) {
+//   console.log(txt[i]);
+// }
+
+// for (const el of txt) {
+//   console.log(el);
+// }

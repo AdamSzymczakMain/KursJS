326616ff800eed35f13c40985f4fbd69c1a82969
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-04-16 12:57:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -127,6 +127,22 @@ function howManydays(month) {
 
 // Tutaj niestety nie byłem w stanie zrobić tego zadania próbowałem a nic nie wychodziło. Niestety będę potrzebować wytłumaczenia całego zadania
 
+// answer
+
+// nadal bez pomocy nie potrafiłem zrobić tego zdania
+function padIt(str, n) {
+  //coding here
+  while (n > 0) {
+    if (n % 2) {
+      str = "*" + str;
+    } else {
+      str = str + "*";
+    }
+    n--;
+  }
+  return str;
+}
+
 //Task 10
 // Funkcja pickItprzyjmuje 1 parametr, arrktóry jest tablicą liczb. Musimy przejść arrza pomocą forpętli. Jeśli element jest liczbą nieparzystą, wypchnij go do tablicy odd, a jeśli jest liczbą parzystą, wypchnij go do eventablicy.
 
@@ -249,4 +265,190 @@ function giveMeFive(obj) {
   return arr;
 }
 
+// Count words
+
+// Can you implement a function that will return number of words in a string?
+
+// You have to ensure that spaces in string is a whitespace for real.
+
+// Let's take a look on some examples:
+
+// countWords("Hello"); // returns 1 as int
+// countWords("Hello, World!") // returns 2
+// countWords("No results for search term `s`") // returns 6
+// countWords(" Hello") // returns 1
+// // ... and so on
+// What kind of tests we made for your code:
+
+// Function have to count words and not spaces. You have to be sure that you doing it right
+// Empty string has no words.
+// String with spaces around should be trimmed.
+// Non-whitespace (ex. breakspace, unicode chars) should be treated as a delimiter
+// Doublecheck that words with chars like -, ', ` are counted right.
+
+// potrzebuje wytłumaczenia
+
+//Task 13Coding in function whatNumberIsIt. function accept 1 parameter:n. it's a number.
+
+// To judge the number n. If n is one of the above five constants, return one of these string:
+
+// "Input number is Number.MAX_VALUE"
+// "Input number is Number.MIN_VALUE"
+// "Input number is Number.NaN"
+// "Input number is Number.NEGATIVE_INFINITY"
+// "Input number is Number.POSITIVE_INFINITY"
+// Other values should return "Input number is xxx". xxx means this number.
+
+// For example:
+
+// whatNumberIsIt(1/0) should return "Input number is Number.POSITIVE_INFINITY"
+// whatNumberIsIt(100) should return "Input number is 100"
+// What you need to think about is how to judge it correctly and effectively and don't forget isNaN().
+
+// answer
+
+function whatNumberIsIt(n) {
+  //coding here
+  if (n === 1.7976931348623157e308) {
+    return "Input number is Number.MAX_VALUE";
+  } else if (n === 5e-324) {
+    return "Input number is Number.MIN_VALUE";
+  } else if (isNaN(n)) {
+    return `Input number is Number.NaN`;
+  } else if (n === -Infinity) {
+    return "Input number is Number.NEGATIVE_INFINITY";
+  } else if (n === Infinity) {
+    return "Input number is Number.POSITIVE_INFINITY";
+  } else return `Input number is ${n}`;
+}
+
+// zadanie rozwiązanie całkie sprawie jednyny kłopoto był przy else if (isNaN(n)) ale udało się wyszukać rozwiązania jak używać isNaN()
+
+// task 14Coding in function colorOf. function accept 3 parameter:r g b. It means value of color red green and blue. the value range is 0-255.
+
+// Use toString(16) Convert numbers r g b to hex string form. at last, combine them to a web color code and return it.
+
+// the color code should starting with "#". and then use 2 characters per color.
+
+// for example:
+
+// colorOf(255,0,0) should return "#ff0000"
+// colorOf(0,111,0) should return "#006f00"
+// colorOf(1, 2 ,3) should return "#010203"
+// That's all of your work. My work is print your color code on your screen.
+
+function colorOf(r, g, b) {
+  //coding here
+  let colorR = r.toString(16).padStart(2, "0");
+  let colorG = g.toString(16).padStart(2, "0");
+  let colorB = b.toString(16).padStart(2, "0");
+
+  let color = colorR + colorG + colorB;
+
+  return "#" + color;
+}
+
+// zadanie zrobione, jedynie potrzebna była pomoc
+// przy padStart() nie wiedziałem dlaczego bez tego nie chciała mu
+//zwrócić poprawengo ciągu.
+
+//task 15
+
+// Coding in function howManySmaller, function accept 2 parameter:arr and n. arr is a decimal array. n is a decimal.
+
+// The first mission: let all elements in the array keep two decimal places(No need to convert number n).
+
+// The second mission: Traversal arr, count the number of the element which smaller than n and return it.
+
+// for example:
+
+// howManySmaller([1.234,1.235,1.228],1.24) should return 2
+// howManySmaller([1.1888,1.1868,1.1838],1.19) should return 1
+// howManySmaller([3.1288,3.1212,3.1205],3.1212) should return 2
+
+//answer
+
+function howManySmaller(arr, n) {
+  //coding here..
+  let twoParameter = arr.map((arr) => arr.toFixed(2));
+  let result = [];
+
+  for (let i = 0; i < twoParameter.length; i++) {
+    if (twoParameter[i] < n) {
+      result.push(twoParameter[i]);
+    }
+  }
+  return result.length;
+}
+
+// udało się cały kod zrobić, miałem tylko na początku problem bo
+//zamiast tablicy w result podstawiłem 0 i zwracało mi błąd.
+
+//task 16
+// Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+// The first mission: Traversing arr, find the shortest string length.
+
+// The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+// for example:
+
+// cutIt(["ab","cde","fgh"]) should return ["ab","cd","fg"]
+// cutIt(["abc","defgh","ijklmn"]) should return ["abc","def","ijk"]
+// cutIt(["codewars","javascript","java"]) should return ["code","java","java"]
+function cutIt(arr) {
+  //coding here...
+  let shortStr = arr[0].length;
+  let shortArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < shortStr) {
+      shortStr = arr[i].length;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    shortArr.push(arr[i].slice(0, shortStr));
+  }
+  return shortArr;
+}
+
+// kod rozwiązany z pomocą, nie wiedziałem jak wyszukać najkrótszy string w tablicy
+
+//task 17
+// Coding in function firstToLast, function accept 2 parameters:str and c. str is a string. c is a char.
+
+// Please return the gap between the first position of c and the last position of c.
+
+// If there are a lot of c in the str, should return a positive integer; If there is only one c in str, should return 0; If there is no c in the str, should return -1. Retrieval should not ignored the case.
+
+// for example:
+
+// firstToLast("ababc","a") should return 2(2-0)
+// firstToLast("ababc","c") should return 0(4-4)
+// firstToLast("ababc","d") should return -1
+
 //
+function firstToLast(str, c) {
+  //coding here..
+  let first = str.indexOf(c);
+  let last = str.lastIndexOf(c);
+
+  if (first >= 0 && last >= 0) {
+    return last - first;
+  } else if (first === -1 && last === -1) return -1;
+  else return 0;
+}
+
+// kod działa poprawnie, ale nie wiem czemu nie działa mi wcześniej gdy miałem zapisałego go w sposób
+
+function firstToLast(str, c) {
+  //coding here..
+  let first = str.indexOf(c);
+  let last = str.lastIndexOf(c);
+
+  if (first >= 0 && last >= 0) {
+    return last - first;
+  } else if (first === last) return 0;
+  else if (first === -1 && last === -1) return -1;
+}

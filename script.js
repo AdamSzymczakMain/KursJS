af7aff6cd883d3e5e7ab597953b341290a361895
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-04-17 15:11:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -452,3 +452,59 @@ function firstToLast(str, c) {
   } else if (first === last) return 0;
   else if (first === -1 && last === -1) return -1;
 }
+
+// task 18
+
+// Implement a function which accepts 2 arguments: string and separator.
+
+// The expected algorithm: split the string into words by spaces, split each word into separate characters and join them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+
+// For example:
+
+// splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+// splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+// splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+// splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+// pierwsza odpowiedź
+function splitAndMerge(string, separator) {
+  return string.split("").join(separator);
+}
+
+// niestety błędna
+function splitAndMerge(string, separator) {
+  return string
+    .split(" ")
+    .map((word) => word.split("").join(separator))
+    .join(" ");
+}
+// bez pomcy był nigdy do tego nie doszedł.
+
+//Task 19
+// o co chodzi z tym /\b\w/g
+// Coding in function alienLanguage, function accept 1 parameter:str. str is a sentence.
+
+// We translate the sentence into an alien language according to the following rules:
+
+// Each word in the sentence is separated by spaces. The last letter of each word in the sentence turns to lowercase, and the other letters are capitalized. Looks very strange? Because this is the form of alien language ;-)
+
+// for example:
+
+// alienLanguage("My name is John") should return "My NAMe Is JOHn"
+// alienLanguage("this is an example") should return "THIs Is An EXAMPLe"
+// alienLanguage("Hello World") should return "HELLo WORLd"
+// A small hint: The first conversion of the entire string will make the code easier
+
+function alienLanguage(str) {
+  //coding here...
+  let upper = str.toUpperCase();
+  let arr = upper.split(" ");
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].slice(0, -1) + arr[i][arr[i].length - 1].toLowerCase();
+  }
+
+  return arr.join(" ");
+}
+
+// niestety nie wiedział jak pętli zrobić aby zamienić ostatnia litere na mała

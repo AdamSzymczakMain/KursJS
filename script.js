4a44ca0ba5c7e577a74554c97282ff0b005c6b89
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-04-24 13:53:49 +0200

    test Task 24

--- script.js
+++ script.js
@@ -710,24 +710,86 @@ function infiniteLoop(arr, d, n) {
   console.log(arr);
 }
 
-infiniteLoop(
-  [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ],
-  "left",
-  1
-);
+// infiniteLoop(
+//   [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ],
+//   "left",
+//   1
+// );
 
-infiniteLoop(
-  [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ],
-  "right",
-  1
-);
+// infiniteLoop(
+//   [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ],
+//   "right",
+//   1
+// );
 
 // zadanie rozwiązane bez większy problemów tylko czy kod w miare czytelny to jest pytanie
+
+//Task 24
+// Coding in function threeInOne. function accept 1 parameters arr, it's a 1D number array. Your task is to merge each of the 3 elements into 1 elements (sum value) and return the result.
+
+// Note1: You should not modify the original array.
+
+// Note2: Because this is a beginner Kata, and due to the author's mercy ;-), so you do not have to verify the validity of the parameter, and do not worry about the number of elements of the array is not a multiple of 3.
+
+// Example:
+
+// threeInOne( [1,2,3]) should return [6]
+// threeInOne( [1,2,3,4,5,6]) should return [6,15]
+// threeInOne( [1,2,3,4,5,6,7,8,9]) should return [6,15,24]
+// threeInOne( [1,3,5,2,4,6,7,7,7]) should return [9,12,21]
+
+//answer
+// function threeInOne(arr) {
+//   //coding here...
+//   let sum = 0;
+//   let result = [];
+
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     sum = sum + arr[i];
+//   }
+//   result.push(sum);
+//   console.log(result);
+// }
+// threeInOne([1, 2, 3]);
+
+// nie działa zgodnie z kata
+function threeInOne(arr) {
+  //coding here...
+  let sum1 = 0;
+  let sum2 = 0;
+  let sum3 = 0;
+  result = [];
+  for (let i = 2; i >= 0; i--) {
+    if (arr.lenght > 6) {
+      let newArr1 = arr.slice(0, 3);
+      let newArr2 = arr.slice(3, 6);
+      let newArr3 = arr.slice(6);
+
+      console.log(arr.slice(3));
+
+      sum1 = sum1 + newArr1[i];
+      sum2 = sum2 + newArr2[i];
+      sum3 = sum3 + newArr3[i];
+    } else if (arr.length > 3) {
+      let newArr1 = arr.slice(0, 3);
+      let newArr2 = arr.slice(3);
+
+      sum1 = sum1 + newArr1[i];
+      sum2 = sum2 + newArr2[i];
+    }
+  }
+  result.push(sum1, sum2, sum3);
+  // console.log(result);
+  return result;
+}
+// console.log(sum1);
+
+threeInOne([1, 2, 3, 4, 5, 6, 7, 8, 9]);

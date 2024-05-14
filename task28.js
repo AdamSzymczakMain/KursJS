737677c1692ca49abd579cac735267fcd07280d1
AdamSzymczakMain <104271530+AdamSzymczakMain@users.noreply.github.com>
2024-05-14 11:12:38 +0200

    edit task 28

---
+++ task28.js
@@ -0,0 +1,20 @@
+// Coding in function mirrorImage. function accept 1 parameter arr, it's a number array. Your task is find the first pair of mirror-image number and return as an array. The two number must be adjacent, and the returned array is in accordance with the order from left to right.
+
+// What's the mirror-image number? for example:123 and 321 is a pair of mirror-image number. Two the same number of palindromes can also be seen as a pair of mirror-image number, such as 121 and 121.
+
+// If no such number is found, return[-1,-1]
+function mirrorImage(arr) {
+  arr.some((x, i) => {
+    a = arr[i];
+    b = arr[i + 1];
+    if (a === b) return a, b;
+    else {
+      for(let i = 0; i < arr[i].length)
+    }
+  });
+  console.log(a, b);
+}
+
+mirrorImage([11, 22, 33, 33, 22, 11]);
+// mirrorImage([454, 86, 57, 75, 16, 88]);
+// mirrorImage([454, 0, 57, 0, 16, 88]);
